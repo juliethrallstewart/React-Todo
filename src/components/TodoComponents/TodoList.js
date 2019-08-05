8cbd0a822d069a93614bd309a25eee15692a0105
Julie Thrall <juliethrall10@gmail.com>
2019-08-05 14:45:12 -0700

    completed add todo list item functionality

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,19 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+import Todo from './Todo.js';
+
+const TodoList = (props) => {
+	// const sortedList = props.toDoList.sort((a, b) => a.purchased - b.purchased);
+	return (
+		<div className="todo-list">
+			{props.toDoList.map((item) => <Todo key={item.id} item={item} toggleItem={props.toggleItem} />)}
+			<button className="clear-btn" onClick={props.clearPurchased}>
+				Clear Completed
+			</button>
+		</div>
+	);
+};
+
+export default TodoList;
